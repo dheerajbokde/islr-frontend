dc76c4f9ac7d3b4720d3f8a4a8546e114d7eeb53
dheeraj.bokde <dheerajbokde21@gmail.com>
2023-04-15 11:19:58 +0530

    image processing modeling

--- pages/Image_Processing.py
+++ pages/Image_Processing.py
@@ -1,13 +1,9 @@
 from PIL import Image
 import numpy as np 
 import streamlit as st 
-
-
-# Function to Read and Manupilate Images
-def load_image(image):
-    img = Image.open(image)
-    img_array = np.array(img)
-    return img_array, img
+from keras.models import load_model
+import keras.utils as image
+import os
 
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
@@ -15,53 +11,59 @@ st.title(':blue[Image Processing]')
 
 col1, col2 = st.columns(2)
 picture = None
+labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z']
 
-def process_image(img):
-    col2.write("IMAGE SIGN TEXT")
+# Function to load an images
+def load_image(image):
+    img = Image.open(image)
+    img_array = np.array(img)
+    return img_array, img
 
 
-# Uploading the File to the Page
-col1.header("Upload Image")
-uploaded_image = col1.file_uploader(label="Upload JPG/PNG Image", type=['jpg', 'png'])
+def process_image(image_name):
+    #print('Process image: ', image_name)
+    model = load_model('src/artifacts/model/cnn_isl_img_model.h5')
+    img = image.load_img(image_name, target_size = (28,28))
+    # convert image into array for prediction
+    test_image = image.img_to_array(img)
+    test_image = np.expand_dims(test_image, axis = 0)
+    # predict image using model
+    result = model.predict(test_image).argmax()
+    #col2.write(labels[result])
+    predicted_text = labels[result]
+    pred_html_str = f"""
+        <style>
+        p.a {{
+            font: bold 45px Courier;
+        }}
+        </style>
+        <p class="a">{predicted_text}</p>
+    """
+    col2.markdown(pred_html_str, unsafe_allow_html=True)
 
-col1.markdown("""---""")
+with col1:
+    st.header("Upload Image")
+    uploaded_image = st.file_uploader(label="Upload JPG/PNG Image", type=['jpg', 'png'])
+    st.markdown("""---""")
+    st.header("Take Picture")
+    captured_image = st.camera_input("First, take a picture...")
 
-col1.header("Take Picture")
-captured_image = col1.camera_input("First, take a picture...")
 
-col2.header("Image for processing")
-if uploaded_image is not None:
-    picture = uploaded_image
-elif captured_image is not None:
-    picture = captured_image
+with col2:
+    st.header("Image Preview")
+    if uploaded_image is not None:
+        picture = uploaded_image
+    elif captured_image is not None:
+        picture = captured_image
 
-if picture is not None: 
-    img_array, img = load_image(picture)
-    col2.write("Image Uploaded Successfully")
-    col2.image(img, use_column_width=True)
-    col2.button('Predict Sign Language',on_click=process_image,args=(img,))
-else:
-    col2.write("Make sure you image is in JPG/PNG Format.")
-
-# Uploading the File to the Page
-#uploadFile = st.file_uploader(label="Upload Image", type=['jpg', 'png'])
-
-# Checking the Format of the page
-# if uploadFile is not None:
-#     img, im = load_image(uploadFile)
-#     # st.image(img)
-#     st.write("Image Uploaded Successfully")
-#     col2.image(im, use_column_width=True)
-
-    # col1, col2 = st.columns(2)
-
-    # col1.header("Original")
-    # col1.file_uploader(label="Upload JPG Image", type=['jpg', 'png'])
-    # #col1.image(img, use_column_width=True)
-
-    # grayscale = im.convert('LA')
-    # picture = col2.camera_input("First, take a picture...")
-    # col2.header("Grayscale")
-    # col2.image(grayscale, use_column_width=True)
-# else:
-#     st.write("Make sure you image is in JPG/PNG Format.")
+    if picture is not None:
+        img_array, img = load_image(picture)
+        # save picture to disk
+        image_name = 'test.jpg'
+        img.save(image_name)
+        #print(img_array)
+        st.write("Image Uploaded Successfully")
+        st.image(img, use_column_width=True)
+        st.button('Predict Sign Language',on_click=process_image,args=(image_name,))
+    else:
+        st.write("Make sure you load image in JPG/PNG format.")
--- pages/Video_Processing.py
+++ pages/Video_Processing.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import cv2
+import cv2
 
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
@@ -21,22 +21,25 @@ with col1:
 def process_video(video):
     col2.write("VIDEO SIGN TEXT")
 
+
 with col2:
-    st.header("Video for processing")
+    st.header("Video Preview")
     if uploaded_video is not None:
         video = uploaded_video
     elif captured_video is not None:
         video = captured_video
 
-
     if video is not None: 
         st.write("Video Uploaded Successfully")
         vid = video.name
         with open(vid, mode='wb') as f:
-            f.write(video.read()) # save video to disk
-            #st.markdown(f"""### Files- {vid}""",unsafe_allow_html=True) # display file name
-            #vidcap = cv2.VideoCapture(vid) # load video from disk
+            # save video to disk
+            f.write(video.read()) 
+            # display file name
+            #st.markdown(f"""### Files- {vid}""",unsafe_allow_html=True)
+            # load video from disk
+            #vidcap = cv2.VideoCapture(vid) 
             st.video(video)
             st.button('Predict Sign Language',on_click=process_video,args=(video,))
     else:
-        st.write("Make sure you load video.")
+        st.write("Make sure you load video in MP4 format.")
