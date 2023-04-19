412a6407f55d85bf63aae72117ac905c3797b3b3
dheeraj.bokde <dheerajbokde21@gmail.com>
2023-04-19 21:44:37 +0530

    video processing

--- pages/Video_Processing.py
+++ pages/Video_Processing.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import cv2
+import cv2
 
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
@@ -11,11 +11,39 @@ video = None
 # Uploading the File to the Page
 with col1:
     st.header("Upload Video")
-    uploaded_video = st.file_uploader("Choose video", type=["mp4"])
+    video = st.file_uploader("Choose video", type=["mp4"])
+    if video is not None:
+        with open(video.name, mode='wb') as f:
+            # save video to disk
+            f.write(video.read()) 
     st.markdown("""---""")
 
-    st.header("Capture Video")
-    captured_video = st.camera_input("First, record a video...")
+    st.header("Record Video")
+    run = st.checkbox('Start recording a video...')
+    while run:
+        cam_video = cv2.VideoCapture(0)
+        frame_size = (640, 480)
+        video_file = 'testvideo.mp4'
+        #fourcc = cv2.VideoWriter_fourcc(*'H264')
+        fourcc = -1
+        writer = cv2.VideoWriter(video_file, fourcc, 20, frame_size)
+        while (cam_video.isOpened()):
+            ret, frame = cam_video.read()
+            if ret:
+                writer.write(frame)
+                window_name = 'Video'
+                cv2.imshow(window_name, frame)
+                cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
+                if cv2.waitKey(1) & 0xFF == 27:
+                    run,ret = False,False
+                    break
+            else: 
+                break
+        cam_video.release()
+        writer.release()
+        cv2.destroyAllWindows()
+        vid = open(video_file, 'rb')
+        video = vid.read()
 
 
 def process_video(video):
@@ -24,22 +52,9 @@ def process_video(video):
 
 with col2:
     st.header("Video Preview")
-    if uploaded_video is not None:
-        video = uploaded_video
-    elif captured_video is not None:
-        video = captured_video
-
     if video is not None: 
         st.write("Video Uploaded Successfully")
-        vid = video.name
-        with open(vid, mode='wb') as f:
-            # save video to disk
-            f.write(video.read()) 
-            # display file name
-            #st.markdown(f"""### Files- {vid}""",unsafe_allow_html=True)
-            # load video from disk
-            #vidcap = cv2.VideoCapture(vid) 
-            st.video(video)
-            st.button('Predict Sign Language',on_click=process_video,args=(video,))
+        st.video(video)
+        st.button('Predict Sign Language',on_click=process_video,args=(video,))
     else:
         st.write("Make sure you load video in MP4 format.")
